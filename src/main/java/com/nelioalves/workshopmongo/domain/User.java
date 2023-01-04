0fab60cdbf9829ea90cf4237883bc12d2b6a63ae
Lenilson Rosa <lenilsonrr86@gmail.com>
2023-01-03 23:03:38 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -2,9 +2,12 @@ package com.nelioalves.workshopmongo.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
 	private String id;
 	private String name;
 	private String email;
